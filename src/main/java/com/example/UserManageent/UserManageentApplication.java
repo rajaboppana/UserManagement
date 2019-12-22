07a9548533d4721827e97a811dc25962f613ff23
root <boppana.deu@gmail.com>
2019-12-22 12:19:54 +0100

    First Commit

---
+++ src/main/java/com/example/UserManageent/UserManageentApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserManageent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManageentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManageentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserManageent/UserManageentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserManageent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManageentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
