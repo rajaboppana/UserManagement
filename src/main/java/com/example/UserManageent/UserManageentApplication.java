e1730bfeac136678073443d68c34c33bfd9bad35
raja <raja@raja-ThinkPad-T470>
2019-12-22 18:39:18 +0100

    Several changes

--- src/main/java/com/example/UserManageent/UserManageentApplication.java
+++ src/main/java/com/example/UserManageent/UserManageentApplication.java
@@ -2,8 +2,10 @@ package com.example.UserManageent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.*")
 public class UserManageentApplication {
 
 	public static void main(String[] args) {
