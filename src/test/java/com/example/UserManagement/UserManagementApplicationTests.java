82b1d6651cc69ce9e558ab81d8557819c825c152
raja <raja@raja-ThinkPad-T470>
2019-12-28 17:26:44 +0100

    Test package name

--- src/test/java/com/example/UserManagement/UserManagementApplicationTests.java
+++ src/test/java/com/example/UserManagement/UserManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.UserManageent;
+package com.example.UserManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserManageentApplicationTests {
+class UserManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
